78639ff44f577ab0dbc314445f2258ad9bda3e9c
bram00648 <113980031+bram00648@users.noreply.github.com>
2024-03-23 15:40:07 +0100

    initial dbt install

---
+++ airbnb/models/time/time.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='external', location='../models/output/time.csv') }}
+
+with dim_time as (
+    {{ dbt_utils.date_spine(
+    datepart="day",
+    start_date="cast('2019-01-01' as date)",
+    end_date="cast('2020-01-01' as date)"
+   )
+}}
+)
+
+SELECT *
+FROM dim_time
